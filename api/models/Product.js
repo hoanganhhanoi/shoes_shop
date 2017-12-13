9cdaa27cd574ae161a13741391194d87c07ecb8f
nguyenhoanganh <nguyenhoanganhbachkhoahanoi@outlook.com.vn>
2017-12-13 23:42:10 +0700

    add model user, product

---
+++ api/models/Product.js
@@ -0,0 +1,19 @@
+/**
+ * Product.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    product_name: {
+      type:"string",
+      required:true
+    },
+    description: {
+      type:"string"
+    }
+  }
+};
